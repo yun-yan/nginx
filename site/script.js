58c523da3d00f65962f96d370de47aa28955ef15
unknown <d187108@gmail.com>
2023-03-20 02:21:08 +1100

    try_js_2

--- site/script.js
+++ site/script.js
@@ -1,104 +1,208 @@
-console.log("Hello World");
-
-var canvas = document.getElementById("canvas");
-var c = canvas.getContext("2d");
-var tx = window.innerWidth;
-var ty = window.innerHeight;
-canvas.width = tx;
-canvas.height = ty;
-//c.lineWidth= 5;
-//c.globalAlpha = 0.5;
-
-var mousex = 0;
-var mousey = 0;
-
-addEventListener("mousemove", function() {
-  mousex = event.clientX;
-  mousey = event.clientY;
-});
+const ee = new EventEmitter();
+const App = React.createClass({ displayName: "App",
+  render() {
+    return /*#__PURE__*/(
+      React.createElement("main", { className: "react-calculator" }, /*#__PURE__*/
+      React.createElement(InputField, null), /*#__PURE__*/
+      React.createElement(TotalRecall, null), /*#__PURE__*/
+      React.createElement(ButtonSetNumbers, null), /*#__PURE__*/
+      React.createElement(ButtonSetFunctions, null), /*#__PURE__*/
+      React.createElement(ButtonSetEquations, null)));
 
 
-var grav = 0.99;
-c.strokeWidth=5;
-function randomColor() {
-  return (
-    "rgba(" +
-    Math.round(Math.random() * 250) +
-    "," +
-    Math.round(Math.random() * 250) +
-    "," +
-    Math.round(Math.random() * 250) +
-    "," +
-    Math.ceil(Math.random() * 10) / 10 +
-    ")"
-  );
-}
+  } });
 
-function Ball() {
-  this.color = randomColor();
-  this.radius = Math.random() * 20 + 14;
-  this.startradius = this.radius;
-  this.x = Math.random() * (tx - this.radius * 2) + this.radius;
-  this.y = Math.random() * (ty - this.radius);
-  this.dy = Math.random() * 2;
-  this.dx = Math.round((Math.random() - 0.5) * 10);
-  this.vel = Math.random() /5;
-  this.update = function() {
-    c.beginPath();
-    c.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
-    c.fillStyle = this.color;
-    c.fill();
-    //c.stroke();
-  };
-}
+const Button = React.createClass({ displayName: "Button",
+  _handleClick() {
+    let text = this.props.text,
+    cb = this.props.clickHandler;
 
-var bal = [];
-for (var i=0; i<50; i++){
-    bal.push(new Ball());
-}
-
-function animate() {    
-  if (tx != window.innerWidth || ty != window.innerHeight) {
-    tx = window.innerWidth;
-    ty = window.innerHeight;
-    canvas.width = tx;
-    canvas.height = ty;
-  }
-  requestAnimationFrame(animate);
-  c.clearRect(0, 0, tx, ty);
-  for (var i = 0; i < bal.length; i++) {
-    bal[i].update();
-    bal[i].y += bal[i].dy;
-    bal[i].x += bal[i].dx;
-    if (bal[i].y + bal[i].radius >= ty) {
-      bal[i].dy = -bal[i].dy * grav;
-    } else {
-      bal[i].dy += bal[i].vel;
-    }    
-    if(bal[i].x + bal[i].radius > tx || bal[i].x - bal[i].radius < 0){
-        bal[i].dx = -bal[i].dx;
+    if (cb) {
+      cb.call(null, text);
     }
-    if(mousex > bal[i].x - 20 && 
-      mousex < bal[i].x + 20 &&
-      mousey > bal[i].y -50 &&
-      mousey < bal[i].y +50 &&
-      bal[i].radius < 70){
-        //bal[i].x += +1;
-        bal[i].radius +=5; 
-      } else {
-        if(bal[i].radius > bal[i].startradius){
-          bal[i].radius += -5;
-        }
-      }
-      
-    //forloop end
+  },
+
+  render() {
+    return /*#__PURE__*/(
+      React.createElement("button", { className: this.props.klass, onClick: this._handleClick }, /*#__PURE__*/
+      React.createElement("span", { className: "title" }, this.props.text)));
+
+
+  } });
+
+const ContentEditable = React.createClass({ displayName: "ContentEditable",
+  _handleClick() {
+    const cb = this.props.clickHandler;
+
+    if (cb) {
+      cb.call(this);
     }
-//animation end
-}
+  },
 
-animate();
+  render() {
+    return /*#__PURE__*/(
+      React.createElement("div", { className: "editable-field", contentEditable: this.props.initEdit, spellcheck: this.props.spellCheck, onClick: this._handleClick },
+      this.props.text));
 
-setInterval(function() {
-  bal.push(new Ball());
-  bal.splice(0, 1);
-}, 400);
+
+  } });
+
+
+const InputField = React.createClass({ displayName: "InputField",
+  _updateField(newStr) {
+    newStr = newStr.split ? newStr.split(' ').reverse().join(' ') : newStr;
+    return this.setState({ text: newStr });
+  },
+
+  getInitialState() {
+    this.props.text = this.props.text || '0';
+
+    return { text: this.props.text };
+  },
+
+  componentWillMount() {
+    ee.addListener('numberCruncher', this._updateField);
+  },
+
+  render() {
+    return /*#__PURE__*/React.createElement(ContentEditable, { text: this.state.text, initEdit: "false", spellcheck: "false", clickHandler: this._clickBait });
+  } });
+
+const TotalRecall = React.createClass({ displayName: "TotalRecall",
+  _toggleMemories() {
+    this.setState({ show: !this.state.show });
+  },
+
+  _recallMemory(memory) {
+    store.newInput = memory;
+    ee.emitEvent('toggle-memories');
+  },
+
+  getInitialState() {
+    return { show: false };
+  },
+
+  componentWillMount() {
+    ee.addListener('toggle-memories', this._toggleMemories);
+  },
+
+  render() {
+    let classNames = `memory-bank ${this.state.show ? 'visible' : ''}`;
+
+    return /*#__PURE__*/(
+      React.createElement("section", { className: classNames }, /*#__PURE__*/
+      React.createElement(Button, { text: "+", clickHandler: this._toggleMemories, klass: "toggle-close" }),
+      store.curMemories.map(mem => {
+        return /*#__PURE__*/(
+          React.createElement(Button, { klass: "block memory transparent", text: mem, clickHandler: this._recallMemory }));
+
+      })));
+
+
+  } });
+
+const ButtonSetFunctions = React.createClass({ displayName: "ButtonSetFunctions",
+  _showMemoryBank() {
+    ee.emitEvent('toggle-memories');
+  },
+
+  _clear() {
+    store.newInput = 0;
+  },
+
+  _contentClear() {
+    let curInput = String(store.curInput),
+    lessOne = curInput.substring(0, curInput.length - 1);
+
+    return store.newInput = lessOne === '' ? 0 : lessOne;
+  },
+
+  render() {
+    return /*#__PURE__*/(
+      React.createElement("section", { className: "button-set--functions" }, /*#__PURE__*/
+      React.createElement(Button, { klass: "long-text", text: "recall", clickHandler: this._showMemoryBank }), /*#__PURE__*/
+      React.createElement(Button, { klass: "long-text", text: "clear", clickHandler: this._clear }), /*#__PURE__*/
+      React.createElement(Button, { text: "\u2190", clickHandler: this._contentClear })));
+
+
+  } });
+
+const ButtonSetEquations = React.createClass({ displayName: "ButtonSetEquations",
+  _eq(type) {
+    store.newInput = `${store.curInput} ${type} `;
+  },
+
+  _equate() {
+    store.newInput = eval(store.curInput);
+  },
+
+  render() {
+    return /*#__PURE__*/(
+      React.createElement("section", { className: "button-set--equations" }, /*#__PURE__*/
+      React.createElement(Button, { text: "+", clickHandler: this._eq }), /*#__PURE__*/
+      React.createElement(Button, { text: "-", clickHandler: this._eq }), /*#__PURE__*/
+      React.createElement(Button, { text: "*", clickHandler: this._eq }), /*#__PURE__*/
+      React.createElement(Button, { text: "/", clickHandler: this._eq }), /*#__PURE__*/
+      React.createElement(Button, { text: "=", clickHandler: this._equate })));
+
+
+  } });
+
+const ButtonSetNumbers = React.createClass({ displayName: "ButtonSetNumbers",
+  _number(num) {
+    if (!store.curInput) {
+      return store.newInput = num;
+    }
+
+    return store.newInput = `${store.curInput}${num}`;
+  },
+
+  render() {
+    return /*#__PURE__*/(
+      React.createElement("section", { className: "button-set--numbers" }, /*#__PURE__*/
+      React.createElement(Button, { text: "1", clickHandler: this._number }), /*#__PURE__*/
+      React.createElement(Button, { text: "2", clickHandler: this._number }), /*#__PURE__*/
+      React.createElement(Button, { text: "3", clickHandler: this._number }), /*#__PURE__*/
+      React.createElement(Button, { text: "4", clickHandler: this._number }), /*#__PURE__*/
+      React.createElement(Button, { text: "5", clickHandler: this._number }), /*#__PURE__*/
+      React.createElement(Button, { text: "6", clickHandler: this._number }), /*#__PURE__*/
+      React.createElement(Button, { text: "7", clickHandler: this._number }), /*#__PURE__*/
+      React.createElement(Button, { text: "8", clickHandler: this._number }), /*#__PURE__*/
+      React.createElement(Button, { text: "9", clickHandler: this._number }), /*#__PURE__*/
+      React.createElement(Button, { text: "0", clickHandler: this._number })));
+
+
+  } });
+
+
+let store = {
+  input: 0,
+  memory: [],
+  get curInput() {
+    return this.input;
+  },
+
+  get curMemories() {
+    return this.memory.filter(m => m !== undefined);
+  },
+
+  set commitMemory(input) {
+    this.memory.push(input);
+  },
+
+  set newInput(str) {
+    let curInput = str,
+    oldInput = this.curInput;
+
+    if (this.curMemories.indexOf(oldInput) === -1) {
+      this.commitMemory = oldInput;
+    }
+
+    this.input = curInput;
+    ee.emitEvent('numberCruncher', [this.curInput]);
+  } };
+
+
+React.render( /*#__PURE__*/
+React.createElement(App, null),
+document.querySelector('body'));
